37cf4cd09e02b2eec695448192a01ec16d3d99df
Kacper Kowalski <xperski@pm.me>
2024-07-20 15:07:32 +0200

    playing sounds

--- main.py
+++ main.py
@@ -1,66 +1,25 @@
 import tkinter as tk
+from settings import *
 from playsound import playsound
+import threading
+
+
+def play_sound(n):
+    threading.Thread(target=playsound, args=(n,), daemon=True).start()
+
 
 root = tk.Tk("Launch Pad", "LaunchPad")
 root.title("Launch Pad")
-root.minsize(600, 600)
-root.maxsize(600, 600)
+root.minsize(600, 700)
+root.maxsize(600, 700)
 
+pixel = tk.PhotoImage(width=1, height=1)
 
-def create_buttons() -> None:
-    COLOR = "aqua"
-    STYLE = tk.SOLID
-    HEIGHT = 100
-    WIDTH = 100
-    PADX = 50 - 3
-    PADY = 50 - 3
-
-    pixel = tk.PhotoImage(width=1, height=1)
-
-    def first_row():
-        button1 = tk.Button(root, command='playsound("notes/a3.mp3")', image=pixel, relief=STYLE)
-        button1.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button1.grid(row=0, column=0, padx=PADX, pady=PADY)
-
-        button2 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button2.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button2.grid(row=0, column=1, padx=PADX, pady=PADY)
-
-        button3 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button3.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button3.grid(row=0, column=2, padx=PADX, pady=PADY)
-
-    def second_row():
-        button4 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button4.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button4.grid(row=1, column=0, padx=PADX, pady=PADY)
-
-        button5 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button5.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button5.grid(row=1, column=1, padx=PADX, pady=PADY)
-
-        button6 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button6.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button6.grid(row=1, column=2, padx=PADX, pady=PADY)
-
-    def third_row():
-        button7 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button7.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button7.grid(row=2, column=0, padx=PADX, pady=PADY)
-
-        button8 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button8.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button8.grid(row=2, column=1, padx=PADX, pady=PADY)
-
-        button9 = tk.Button(root, command=root.destroy, image=pixel, relief=STYLE)
-        button9.config(height=HEIGHT, width=WIDTH, bg=COLOR)
-        button9.grid(row=2, column=2, padx=PADX, pady=PADY)
-
-    first_row()
-    second_row()
-    third_row()
-
-
-create_buttons()
+for row in range(ROWS):
+    for col in range(COLUMNS):
+        note = notes[(row * COLUMNS) + col]
+        button = tk.Button(root, command=lambda n=note: play_sound(n), image=pixel, relief=STYLE)
+        button.config(height=HEIGHT, width=WIDTH, bg=COLOR)
+        button.grid(row=row, column=col, padx=PADX, pady=PADY)
 
 root.mainloop()
---
+++ settings.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+COLUMNS = 4
+ROWS = 4
+COLOR = "aqua"
+STYLE = tk.SOLID
+HEIGHT = 100
+WIDTH = 100
+PADX = 50 - 3
+PADY = 25 - 3
+
+notes = [
+    'notes/a3.mp3', 'notes/a4.mp3', 'notes/a5.mp3',
+    'notes/b3.mp3', 'notes/b5.mp3', 'notes/d3.mp3',
+    'notes/d4.mp3', 'notes/c5.mp3', 'notes/c6.mp3',
+    'notes/e3.mp3', 'notes/e4.mp3', 'notes/e5.mp3',
+    'notes/f3.mp3', 'notes/f4.mp3', 'notes/f5.mp3',
+    'notes/g3.mp3', 'notes/g4.mp3', 'notes/g5.mp3',
+]
